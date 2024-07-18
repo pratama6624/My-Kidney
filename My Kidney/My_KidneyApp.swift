4f1a717aa1c4ca74da91f8d077cc9b95753de609
pratama6624 <scyoung6624@gmail.com>
2024-07-18 22:31:43 +0700

    Initial Commit

---
+++ My Kidney/My_KidneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_KidneyApp.swift
+//  My Kidney
+//
+//  Created by Pratama One on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct My_KidneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
